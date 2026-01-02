6446384f9e61957c75f6086a56303b207f775d5e
Saga-Academy <awdtph@gmail.com>
2026-01-02 20:46:47 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase, ref, push, onChildAdded, onValue, set, remove, runTransaction } 
+import { getDatabase, ref, push, onChildAdded, onValue, set, remove }
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } 
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut }
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
@@ -19,7 +19,6 @@ const db = getDatabase(app);
 const auth = getAuth(app);
 
 export {
-  db, ref, push, onChildAdded, onValue,
-  set, remove, runTransaction,
-  auth, RecaptchaVerifier, signInWithPhoneNumber
+  db, ref, push, onChildAdded, onValue, set, remove,
+  auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut
 };
