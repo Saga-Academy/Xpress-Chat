fe898a25f1a740bded61eaa76cf7caef5ce0a06f
Saga-Academy <awdtph@gmail.com>
2026-01-02 19:10:20 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -5,10 +5,14 @@ import { getAuth, RecaptchaVerifier, signInWithPhoneNumber }
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT"
+  apiKey: "AIzaSyA60ZFublVIZyAZnIFLM-nUQmDd4l-84ko",
+  authDomain: "xpress-chat-b2269.firebaseapp.com",
+  // 🔥 FIXED: This must be the database endpoint, not the console link
+  databaseURL: "https://xpress-chat-b2269-default-rtdb.firebaseio.com", 
+  projectId: "xpress-chat-b2269",
+  storageBucket: "xpress-chat-b2269.firebasestorage.app",
+  messagingSenderId: "734261007530",
+  appId: "1:734261007530:web:1d5dc5b9c8ca16dd5bacd3",
 };
 
 const app = initializeApp(firebaseConfig);
