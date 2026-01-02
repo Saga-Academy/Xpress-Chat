854ba42276541824917a545bfc9f09c8702c2802
Saga-Academy <awdtph@gmail.com>
2026-01-02 19:38:13 +0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,39 +1,25 @@
-import { auth, RecaptchaVerifier, signInWithPhoneNumber, db, ref, set }
-from "./firebase.js";
+import { auth, RecaptchaVerifier, signInWithPhoneNumber, db, ref, set } from "./firebase.js";
 
 let confirmation;
+const phoneInput = document.getElementById("phone");
+const otpInput = document.getElementById("otp");
+const avatarInput = document.getElementById("avatar");
 
-window.recaptchaVerifier = new RecaptchaVerifier(
-  "recaptcha",{size:"invisible"},auth
-);
+window.recaptchaVerifier = new RecaptchaVerifier("recaptcha", { size: "invisible" }, auth);
 
 window.sendOTP = () => {
-  const phone = phoneInput.value;
-  signInWithPhoneNumber(auth, phone, window.recaptchaVerifier)
-  .then(r => { confirmation=r; alert("OTP sent"); });
+  signInWithPhoneNumber(auth, phoneInput.value, window.recaptchaVerifier)
+    .then(r => { confirmation = r; alert("Access Code Transmitted"); })
+    .catch(err => alert("Transmission Failed: " + err.message));
 };
 
 window.verifyOTP = () => {
-  confirmation.confirm(otp.value).then(res=>{
-    const avatar =
-      avatarInput.value ||
-      `https://api.dicebear.com/7.x/initials/svg?seed=${phoneInput.value}`;
-
-    localStorage.setItem("uid",res.user.uid);
-    localStorage.setItem("user",phoneInput.value);
-    localStorage.setItem("avatar",avatar);
-    localStorage.setItem("room","general");
-
-    set(ref(db,"users/"+res.user.uid),{
-      user:phoneInput.value,
-      avatar,
-      online:true
-    });
-
-    location.href="index.html";
+  confirmation.confirm(otpInput.value).then(res => {
+    const avatar = avatarInput.value || `https://api.dicebear.com/7.x/bottts/svg?seed=${phoneInput.value}`;
+    localStorage.setItem("uid", res.user.uid);
+    localStorage.setItem("user", phoneInput.value);
+    localStorage.setItem("avatar", avatar);
+    set(ref(db, "users/" + res.user.uid), { user: phoneInput.value, avatar, online: true });
+    location.href = "index.html";
   });
 };
-
-const phoneInput=document.getElementById("phone");
-const otp=document.getElementById("otp");
-const avatarInput=document.getElementById("avatar");
